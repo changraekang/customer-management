cc33cadb0f2747f60ef95290f09d4017ff0b96d5
changrae <storkcr@gmail.com>
2022-02-14 13:38:22 +0900

    meterial ui 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import Customer from "./components/Customer";
 import "./App.css";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+  },
+  table: {
+    minwidth: 1000,
+  },
+});
 
 function App() {
   return (
-    <div>
-      <Customer />
-    </div>
+    <Paper className={styles.root}>
+      <Table className={styles.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <Customer />
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
